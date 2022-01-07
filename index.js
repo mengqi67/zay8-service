a86d7384b27022d7654b042bcb289cd6ee96858c
yimengqi <yimengqi@rong360.com>
2022-01-07 14:50:15 +0800

    page

--- index.js
+++ index.js
@@ -1,26 +1,46 @@
 /*
  * @Author: ymq
  * @Date: 2022-01-05 18:50:55
- * @LastEditTime: 2022-01-05 19:09:36
+ * @LastEditTime: 2022-01-06 19:09:16
  * @LastEditors: ymq
  * @Description: 
  */
 const http = require('http');
-const fs = require('fs')
+const fs = require('fs');
+const express = require('express')
+const path = require('path')
+
+
 const hostname = '0.0.0.0';
 const port = 443;
-const server = http.createServer((req, res) => {
-    if (req.url == '/') {
-        fs.readFile('./index.html','utf-8', (error, data) => {
+const app = express()
+
+// const server = http.createServer((req, res) => {
+//     if (req.url == '/') {
+//         fs.readFile('./index.html','utf-8', (error, data) => {
+//             res.statusCode = 200;
+//             res.setHeader('Content-Type', 'text/html');
+//             res.end(data);
+//         })
+        
+//     }
+    
+// });
+
+// server.listen(port, hostname, () => {
+//     console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+app.use(express.static(__dirname))
+
+app.all('/', (req, res) => {
+    if(req.url === '/') {
+        fs.readFile('./index.html', 'utf-8', (error, data) => {
             res.statusCode = 200;
             res.setHeader('Content-Type', 'text/html');
             res.end(data);
         })
-        
     }
-    
-});
+})
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port)
