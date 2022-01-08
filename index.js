ceafd5f9317155ea1dcaf70e08b8c7ff5e661cfc
yimengqi <yimengqi@rong360.com>
2022-01-08 16:42:11 +0800

    在线聊天室done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: ymq
  * @Date: 2022-01-05 18:50:55
- * @LastEditTime: 2022-01-06 19:09:16
+ * @LastEditTime: 2022-01-08 16:21:40
  * @LastEditors: ymq
  * @Description: 
  */
@@ -9,11 +9,15 @@ const http = require('http');
 const fs = require('fs');
 const express = require('express')
 const path = require('path')
+const expressWs = require('express-ws')
 
 
 const hostname = '0.0.0.0';
 const port = 443;
+const userIndex = 0;
 const app = express()
+const wxInstance = expressWs(app)
+const wss = wxInstance.getWss()
 
 // const server = http.createServer((req, res) => {
 //     if (req.url == '/') {
@@ -43,4 +47,29 @@ app.all('/', (req, res) => {
     }
 })
 
-app.listen(port)
+app.ws('/chat', (ws, req) => {
+    console.log('有聊天用户链接上来');
+    // console.log(req);
+    // ws.userId = ++userIndex
+    
+    // console.log(ws.userId);
+    ws.on('message', function (msg) {
+        msg = JSON.parse(msg)
+        if(msg.type == 'message') {
+            const date = new Date()
+            msg.value['time'] = `${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}`
+        }
+        broadcast(JSON.stringify(msg))
+    })
+
+})
+
+function broadcast(msg) {
+    wss.clients.forEach(item => {
+        item.send(msg)
+    })
+}
+
+app.listen(port, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+})
